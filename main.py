276dd5c5e02dcfbdf11ed460a03b220d4fb458cc
Eazy-PZ <notreallyezg@gmail.com>
2025-12-09 18:08:37 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+##################### Extra Hard Starting Project ######################
+
+# 1. Update the birthdays.csv
+
+# 2. Check if today matches a birthday in the birthdays.csv
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+# 4. Send the letter generated in step 3 to that person's email address.
+
+
+import pandas as pd
+import datetime as dt
+import random
+import smtplib
+import os
+from dotenv import load_dotenv
+
+load_dotenv('day32/.env')
+
+EMAIL = os.getenv('email')
+PASSWORD = os.getenv('password')
+
+birthday_file = pd.read_csv('day32/birthday-wisher-extrahard-start/birthdays.csv')
+now = dt.datetime.now()
+year, month, day = now.year, now.month, now.day
+
+
+birthday_info = {value.person : [value.year, value.month, value.day] for (key, value) in birthday_file.iterrows()}
+
+for name in birthday_info:
+    if birthday_info[name] == [year, month, day]:
+        print('hey')
+
+        PLACEHOLDER = "[NAME]"
+        letter1 = 'day32/birthday-wisher-extrahard-start/letter_templates/letter_1.txt'
+        letter2 = 'day32/birthday-wisher-extrahard-start/letter_templates/letter_2.txt'
+        letter3 = 'day32/birthday-wisher-extrahard-start/letter_templates/letter_3.txt'
+
+
+        with open(random.choice([letter1, letter2, letter3])) as letter_file:
+            letter_contents = letter_file.read()
+            new_letter = letter_contents.replace(PLACEHOLDER, name)
+            with open(f"day32/birthday-wisher-extrahard-start/letter_for_{name}.txt", mode="w") as completed_letter:
+                completed_letter.write(new_letter)
+
+
+        message = f'subject: Happy Birthday\n\n {new_letter}'
+        
+        with smtplib.SMTP_SSL(os.getenv('smtp'), os.getenv('port')) as connection:
+            connection.login(EMAIL, PASSWORD)
+            connection.sendmail(EMAIL, "examplemail@gmail.com", message)
